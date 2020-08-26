34c21a20fc9167c2d728fb966d4d3d026f340880
Hammad Imran <Chhamad928@gmail.com>
2020-08-26 16:23:05 +0500

    Hi This is my todays Work ! Mean example of Lifo

---
+++ Class Task-1 Stack(LIFO).py
@@ -0,0 +1,26 @@
+foldersList = []
+
+foldersList.append("mangoFolder")
+foldersList.append("oranageFolder")
+foldersList.append("bannanaFolder")
+foldersList.append("guavaFolder")
+foldersList.append("cherryFolder")
+
+print(foldersList)
+
+foldersList.pop()
+print(foldersList)
+
+foldersList.pop()
+print(foldersList)
+
+foldersList.pop()
+print(foldersList)
+
+foldersList.pop()
+print(foldersList)
+
+foldersList.pop()
+print(foldersList)
+
+foldersList.pop()
